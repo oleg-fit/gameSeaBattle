faeae26a3402485072b4efb65fed320d913361a7
oleg-fit <oleg26-spb@rambler.ru>
2020-03-04 10:49:47 +0300

    Добавляет реализацию счетчика выстрелов и реализацию отображения промахов на игровом поле.

---
+++ script.js
@@ -0,0 +1,54 @@
+const record = document.getElementById('record');
+const shot = document.getElementById('shot');
+const hit = document.getElementById('hit');
+const dead = document.getElementById('dead');
+const enemy = document.getElementById('enemy');
+const again = document.getElementById('again');
+
+const play = {
+    record: 0,
+    shot: 0,
+    hit: 0,
+    hit: 0,
+    set updateData(data) {
+        this[data] += 1;
+        this.render();
+    },
+    render() {
+        record.textContent = this.record;
+        shot.textContent = this.shot;
+        hit.textContent = this.hit;
+        dead.textContent = this.hit;
+    }
+};
+
+const show = {
+    hit() {
+
+    },
+    miss(elem) {
+        this.changeClass(elem, 'miss');
+    },
+    dead() {
+        
+    },
+    changeClass(elem, value) {
+        elem.className = value;
+    } 
+};
+
+const fire = (event) => {
+    const target = event.target;
+    show.miss(target);
+
+    // Проверка на повторное событие. Не понимаю почему не работает!
+    if (!event.target.classList.contains('miss')) {
+        play.updateData = 'shot';
+    }
+}
+
+const init = () => {
+    enemy.addEventListener('click', fire);
+};
+
+init();
